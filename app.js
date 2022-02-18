8f992142cd09f8e8cb30a6aa345828ef9bd94e3b
Koray <example>
2022-02-18 15:38:26 +0300

    Dosya yapısı düzenlendi

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require("express")
+const ejs = require('ejs');
 
 const app = express()
 
+
+//Template Engine(Şablon Motoru :) )
+app.set("view engine", "ejs")
+
+
+//Middleware(Ara Yazılımlar)
+app.use(express.static("public"))
+
+
+//Routes(Yönlendirmeler)
 app.get("/", (req, res) => {
-  res.status(404).send("Merhaba Dünya")
+  res.status(200)
+  .render("index",{
+      page_name:"index"
+  })
 })
+app.get("/about", (req, res) => {
+  res.status(200)
+  .render("about",{
+      page_name:"about"
+  })
+})
+app.get("/contact", (req, res) => {
+  res.status(200)
+  .render("contact")
+})
+
+
+
 
 //Server Başlatılıyor..
 const port = 5200
