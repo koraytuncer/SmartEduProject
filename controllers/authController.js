20205e97741cb28440f00483361ff76095183b1d
Koray <you@example.com>
2022-02-20 06:52:36 +0300

    Özel middleware yazıldı

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,7 @@ exports.loginUser = (req, res) => {
           if (same) {
             // USER SESSION
             req.session.userID = user._id
-            res.status(200).redirect("/")
+            res.status(200).redirect("/users/dashboard")
           }
         })
       }
@@ -38,4 +38,18 @@ exports.loginUser = (req, res) => {
       error,
     })
   }
+}
+
+exports.logoutUser = (req, res) => {
+  req.session.destroy(()=> {
+    res.redirect('/');
+  })
+}
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id:req.session.userID})
+  res.status(200).render("dashboard", {
+    page_name: "dashboard",
+    user
+  })
 }
